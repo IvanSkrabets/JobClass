231721968c9e3bb47a4643aecb09c45c185fae89
we100ka@gmail.com <we100ka@yandex.by>
2022-05-25 22:21:23 +0300

    dafbf

---
+++ src/main/java/by/tms/servlet/entity/User.java
@@ -0,0 +1,40 @@
+package by.tms.servlet.entity;
+
+public class User {
+    private String name;
+    private String userName;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
